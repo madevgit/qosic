1cd27f29506565d7ba8960d18de58c9e91126c6f
madevgit <moukadimalassani@gmail.com>
2021-08-26 12:35:18 +0100

    hashtype

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, MemoryRouter } from "react-router-dom";
+import { HashRouter as Router, MemoryRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
 import "./prism-night-owl.css";
 import "react-intl-tel-input/dist/main.css";
