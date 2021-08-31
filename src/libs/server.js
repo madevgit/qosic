9ece74b344af9bcc8359fc262c25cbc69d185693
aremou1010 <aremou1010@gmail.com>
2021-08-31 18:53:33 +0100

    update config

--- src/libs/server.js
+++ src/libs/server.js
@@ -1,2 +1,3 @@
-const baseUrl = "https://blogeditorapi.herokuapp.com";
+//const baseUrl = "https://blogeditorapi.herokuapp.com";
+const baseUrl = "http://74.208.84.251/qos-blog-api-test";
 export default baseUrl;
